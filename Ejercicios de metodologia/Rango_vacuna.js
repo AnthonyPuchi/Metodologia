74585e34779c4909677ce528cb38013bbecce800
Anthony <108705558+AnthonyPuchi@users.noreply.github.com>
2023-01-17 20:50:39 -0500

    Ejercicios metodologia
    Ejercicios resultos

---
+++ Ejercicios de metodologia/Rango_vacuna.js
@@ -0,0 +1,10 @@
+//Escribir un programa que a permita validad la edad para recibir una vacuna. 
+//Las edades permitidas son menores de 6 y mayores de 65
+
+let agevalid = 50;
+
+if (agevalid < 6 || agevalid > 65) {
+    console.log("Edad valida para la vacuna")
+} else {
+    console.log("Edad no valida para la vacuna")
+}
---
+++ Ejercicios de metodologia/login.js
@@ -0,0 +1,8 @@
+let password ="1218";
+let username= "Anthony"
+
+if(password == "1218" && username == "Anthony"){
+    console.log("Bienvenido " + username + " tu clave es " + password);
+}else{
+    console.log("clave incorrecta, ingrese de nuevo ");
+}
